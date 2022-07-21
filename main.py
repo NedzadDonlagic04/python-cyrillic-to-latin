ef13d7782e90c085fe76b79868a4c46150c76134
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-07-22 00:28:52 +0200

    Python
    Imported the package used for translation from
    latin to cyrillic, after which I used the
    provided functions to perform the translation.
    As a last step I added the function to let the
    user input the cyrillic.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import cyrtranslit as cyr
+
 def main():
-    print('Hello world!')
+    text=input('Enter some cyrillic: ')
+    text=cyr.to_latin(text,'ru')
+    print('Translated to latin:',text)
 
 if __name__ == '__main__':
     main()
