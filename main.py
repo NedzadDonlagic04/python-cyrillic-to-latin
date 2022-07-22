bc6cf8fabfaafce9a95dbcb6b622b42e0852241d
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-07-22 16:00:20 +0200

    Python
    Imported the package used for auto correcting
    text,configured it to only do it for english.
    As a last step I added it to the output
    with the cyrillic to latin part.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import cyrtranslit as cyr
+from autocorrect import Speller
 
 def main():
     text=input('Enter some cyrillic: ')
     text=cyr.to_latin(text,'ru')
-    print('Translated to latin:',text)
+
+    spell=Speller()
+    
+    print('Translated to latin:',spell(text))
 
 if __name__ == '__main__':
     main()
