286090f83ecc4e05576a2b14d40f2dcc8f48564e
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-07-22 18:22:44 +0200

    Python
    Imported the package used for CLI argument
    passing,made it by default use Serbian or
    su due to the method to_latin by default
    being in Serbian. Using the flags -l or
    --lang you can specify one of the other
    languages supported by the package as well
    as a way to throw an error in case the
    language doesn't exist.

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import cyrtranslit as cyr
+import argparse as arg
+
 from autocorrect import Speller
 
 def main():
+    parser=arg.ArgumentParser(description='Cyrillic to latin converter')
+    parser.add_argument('-l','--lang',
+                        help='Specifies the language used for the translation',
+                        default='sr')
+    args=parser.parse_args()
+    lang=args.lang
+    
+    langList=cyr.supported()
+
+    if lang not in langList:
+        print('Language isn\'t supported')
+        quit()
+
     text=input('Enter some cyrillic: ')
-    text=cyr.to_latin(text,'ru')
+    text=cyr.to_latin(text,lang)
 
     spell=Speller()
-    
+
     print('Translated to latin:',spell(text))
 
 if __name__ == '__main__':
